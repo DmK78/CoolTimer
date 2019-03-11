de5e4bd6264381e151da864f916041d709d77890
DmK78 <Ek193burg>
2019-03-11 14:25:30 +0500

    finish

--- app/src/main/java/com/dmk78/cooltimer/MainActivity.java
+++ app/src/main/java/com/dmk78/cooltimer/MainActivity.java
@@ -1,13 +1,119 @@
 package com.dmk78.cooltimer;
 
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private SeekBar seekBar;
+    private TextView textView;
+    private boolean isTimerOn;
+    private Button button;
+    private CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar = findViewById(R.id.seekBar);
+        textView = findViewById(R.id.textView);
+
+        seekBar.setMax(600);
+        seekBar.setProgress(30);
+        isTimerOn = false;
+        button = findViewById(R.id.button);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                long progressInMillis = progress * 1000;
+                updateTimer(progressInMillis);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+    }
+
+    public void start(View view) {
+
+        if (!isTimerOn) {
+
+            button.setText("pause");
+            seekBar.setEnabled(false);
+            isTimerOn = true;
+
+            countDownTimer = new CountDownTimer(seekBar.getProgress() * 1000, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+
+                    updateTimer(millisUntilFinished);
+
+                }
+
+                @Override
+                public void onFinish() {
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.bell_sound);
+                    mediaPlayer.start();
+                    resetTimer();
+
+                }
+            };
+            countDownTimer.start();
+
+        } else {
+
+            resetTimer();
+        }
+
+    }
+
+    private void updateTimer(long millisUntilFinished) {
+
+        int minutes = ((int) millisUntilFinished / 1000) / 60;
+        int seconds = ((int) millisUntilFinished / 1000) - (minutes * 60);
+        String minutesString = "";
+        String secondsString = "";
+        if (minutes < 10) {
+            minutesString = "0" + minutes;
+        } else {
+            minutesString = "" + minutes;
+        }
+        if (seconds < 10) {
+            secondsString = "0" + seconds;
+        } else {
+            secondsString = "" + seconds;
+        }
+        textView.setText(minutesString + ":" + secondsString);
+
+    }
+
+    private void resetTimer(){
+        countDownTimer.cancel();
+        textView.setText("00:30");
+        button.setText("Play");
+        seekBar.setEnabled(true);
+        seekBar.setProgress(30);
+        isTimerOn = false;
+
     }
 }
